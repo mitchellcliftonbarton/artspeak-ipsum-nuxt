e965076b0f01296a3794de81fa7fe810e8c73664
Mitchell Barton <mitchell@welikesmall.com>
2022-05-03 17:12:35 -0600

    adding social stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,66 @@
+const description = 'A simple lorem ipsum generator based on International Art English.'
+const title = 'Artspeak Ipsum'
+const socialImage = './social.jpg'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Artspeak Ipsum',
+    title: title,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: description },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: title
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: description
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: socialImage
+      },
+      {
+        hid: 'twitter:image:alt',
+        name: 'twitter:image:alt',
+        content: title
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: title
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: description
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: socialImage
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: socialImage
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: title
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -29,6 +78,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,6 +92,13 @@ export default {
     manifest: {
       lang: 'en',
     },
+    icon: {
+      fileName: 'favicon.png'
+    }
+  },
+
+  googleAnalytics: {
+    id: 'UA-61480746-10'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
